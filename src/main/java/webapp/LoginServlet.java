c035f834fd5013f3b6ff9848be2ce6b79c366a79
firas <firastayeb1@gmail.com>
2019-11-17 21:58:36 +0100

    first pipeline

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<title>Yahoo!!!!!!!!</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("My First Servletttttttttt");
+		out.println("My First Servletttttttt");
 		out.println("</body>");
 		out.println("</html>");
 
