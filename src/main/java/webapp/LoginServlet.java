ef27b6d80d4b4932b59db339d510df0b8e0e49b5
firas <firastayeb1@gmail.com>
2019-12-06 22:41:53 +0100

    test2

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -40,7 +40,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("<title>Yahooo</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("My First Servlettttttttttttttttttttttt");
+		out.println("My First Servlethhhhhhhhhhhh");
 		out.println("</body>");
 		out.println("</html>");
 
